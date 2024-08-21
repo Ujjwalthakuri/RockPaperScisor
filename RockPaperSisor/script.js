004f4b21056fd1dcc0e70ddbac5f1bd389335955
Ujjwal Thakuri <ujjwalthakuri46@gmail.com>
2024-08-21 15:28:51 +0545

    score and message display!

--- RockPaperSisor/script.js
+++ RockPaperSisor/script.js
@@ -4,19 +4,34 @@ let com_score=0;
 
 let choices= document.querySelectorAll(".rps");
 
+let result=document.getElementById("res");
+
+let userscore = document.getElementById("user_score");
+let computerscore = document.getElementById("sys_score");
+
 let draw=()=>{
-    console.log("game is draw");
+    // console.log("game is draw");
+    result.innerText="Game is draw";
 };
 
-let display_winner=(user_win)=>{
+let display_winner=(user_win, rps_Id, com_gen)=>{
     if(user_win){
-        console.log("You Win");
+       user_score++
+        userscore.innerText=user_score;
+        // console.log("You Win");
+        result.innerText=`You win because ${rps_Id} beats ${com_gen} `;
+
     }else{
-        console.log("You Lose");
+        com_score++;
+        computerscore.innerText = com_score;
+        // console.log("You Lose");
+        result.innerText = `You win because ${rps_Id} cannot beats ${com_gen} `;
     }
 };
 
 let computerchoice=()=>{
+    // console.log("Wait computer is choosing");
+    // result.innerText="Computer is choosing";
     let option = ["rock", "paper", "scissor"];
     let randomNum=Math.floor(Math.random()*3);
     return option[randomNum];
@@ -38,7 +53,7 @@ let playgame = (rps_Id) => {
         }else if(rps_Id==="scissor"){
             user_win= com_gen==="rock"?flase:true;
         }
-        display_winner(user_win);
+        display_winner(user_win, rps_Id, com_gen);
     }
     
 };
